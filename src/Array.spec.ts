3aa6d5996c7ae6c3e47cb94305e705d36fd30148
Dawid Pazik <dawidpazik@gmail.com>
2019-10-02 21:20:20 +0200

    add except, first, firstOrUndefined, groupBy, groupJoin and innerJoin methods

--- src/Array.spec.ts
+++ src/Array.spec.ts
@@ -1,12 +1,391 @@
 import "Array.ts";
 
-describe("where", () => {
-    test("returns an array with elements that satisfy the given condition", () => {
+describe("array", () => {
+    test("aggregate returns correct aggregated value transformed with a given result selector", () => {
+        const array = [{ key: "1", value: 10 }, { key: "2", value: 20 }, { key: "3", value: 30 }, { key: "4", value: 40 }];
+        const seed = 1;
+        const func = (accumulator: number, element: { value: number }) => accumulator + element.value;
+        const resultSelector = (accumulator: number) => accumulator + 3;
+
+        const result = array.aggregate(seed, func, resultSelector);
+
+        expect(result).toBe(104);
+    });
+
+    test("all returns true if all array elements satisfy a given condition", () => {
+        const array = [4, 5, 6, 7];
+        const predicate = (element: number) => element > 3;
+
+        const result = array.all(predicate);
+
+        expect(result).toBeTruthy();
+    });
+
+    test("all returns false if any array element doesn't satisfy a given condition", () => {
+        const array = [4, 5, 6, 7, 3];
+        const predicate = (element: number) => element > 3;
+
+        const result = array.all(predicate);
+
+        expect(result).toBeFalsy();
+    });
+
+    test("any returns true if any of array elements satisfies a given condition", () => {
+        const array = [1, 2, 3, 4];
+        const predicate = (element: number) => element > 3;
+
+        const result = array.any(predicate);
+
+        expect(result).toBeTruthy();
+    });
+
+    test("any returns false if none of array elements satisfies a given condition", () => {
+        const array = [1, 2, 3];
+        const predicate = (element: number) => element > 3;
+
+        const result = array.any(predicate);
+
+        expect(result).toBeFalsy();
+    });
+
+    test("append returns a new array with a given element appended", () => {
+        const array = [1, 2, 3];
+
+        const result = array.append(4);
+
+        expect(result).toEqual([1, 2, 3, 4]);
+        expect(result).not.toBe(array);
+    });
+
+    test("average returns average of array elements transformed with a given selector", () => {
+        const array = [{ key: "1", value: 10 }, { key: "2", value: 20 }, { key: "3", value: 30 }, { key: "4", value: 40 }];
+        const selector = (element: { value: number }) => element.value;
+
+        const result = array.average(selector);
+
+        expect(result).toBe(25);
+    });
+
+    test("average returns average of array elements when a selector is not provided and all array elements are of number type", () => {
+        const array = [10, 20, 30, 40];
+
+        const result = array.average();
+
+        expect(result).toBe(25);
+    });
+
+    test("average throws an error when a selector is not provided and not all array elements are of number type", () => {
+        const array = [10, 20, 30, 40, "a"];
+
+        const action = () => array.average();
+
+        expect(action).toThrowError("All array elements must be numbers");
+    });
+
+    test("average throws an error when the array is empty", () => {
+        const array = [] as Array<number>;
+
+        const action = () => array.average();
+
+        expect(action).toThrowError("Sequence contains no elements");
+    });
+
+    test("contains returns true if a given element is present in the array", () => {
+        const array = [1, 2, 3, 4];
+
+        const result = array.contains(3);
+
+        expect(result).toBeTruthy();
+    });
+
+    test("contains returns false if a given element is present in the array", () => {
+        const array = [1, 2, 3, 4];
+
+        const result = array.contains(5);
+
+        expect(result).toBeFalsy();
+    });
+
+    test("count returns the array length", () => {
+        const array = [1, 2, 3, 4];
+
+        const result = array.count();
+
+        expect(result).toBe(4);
+    });
+
+    test("defaultIfEmpty returns array with a given default element when the source array is empty", () => {
+        const array = [] as Array<number>;
+
+        const result = array.defultIfEmpty(3);
+
+        expect(result).toEqual([3]);
+    });
+
+    test("defaultIfEmpty returns the source array when it's not empty", () => {
+        const array = [1, 2];
+
+        const result = array.defultIfEmpty(3);
+
+        expect(result).toBe(array);
+    });
+
+    test("distinct returns a new array with distinct values", () => {
+        const array = [1, 1, 2, 3, 3, 4, 5, 5];
+
+        const result = array.distinct();
+
+        expect(result).toEqual([1, 2, 3, 4, 5]);
+        expect(result).not.toBe(array);
+    });
+
+    test("elementAt returns element at a specified index", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const result = array.elementAt(3);
+
+        expect(result).toBe(4);
+    });
+
+    test("elementAt throws when a specified index is less than 0", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const action = () => array.elementAt(-1);
+
+        expect(action).toThrowError("Index out of range");
+    });
+
+    test("elementAt throws when a specified index is greater than the array length", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const action = () => array.elementAt(6);
+
+        expect(action).toThrowError("Index out of range");
+    });
+
+    test("elementAt throws when a specified index is equal to the array length", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const action = () => array.elementAt(5);
+
+        expect(action).toThrowError("Index out of range");
+    });
+
+    test("elementAtOrUndefined returns element at a specified index", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const result = array.elementAtOrUndefined(3);
+
+        expect(result).toBe(4);
+    });
+
+    test("elementAtOrUndefined returns undefined when a specified index is less than 0", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const result = array.elementAtOrUndefined(-1);
+
+        expect(result).toBe(undefined);
+    });
+
+    test("elementAtOrUndefined returns undefined when a specified index is greater than the array length", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const result = array.elementAtOrUndefined(6);
+
+        expect(result).toBe(undefined);
+    });
+
+    test("elementAtOrUndefined returns undefined when a specified index is equal to the array length", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const result = array.elementAtOrUndefined(5);
+
+        expect(result).toBe(undefined);
+    });
+
+    test("except returns a new array with those items from the first array that aren't present in the second one", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const result = array.except([2, 4]);
+
+        expect(result).toEqual([1, 3, 5]);
+        expect(result).not.toBe(array);
+    });
+
+    test("first returns first element of the array when the array contains elements and no predicate is provided", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const result = array.first();
+
+        expect(result).toBe(1);
+    });
+
+    test("first returns first element of the array that satisfied given predicate when such an element exists", () => {
+        const array = [1, 2, 3, 4, 5];
+        const predicate = (element: number) => element > 3;
+
+        const result = array.first(predicate);
+
+        expect(result).toBe(4);
+    });
+
+    test("first throws when the array is empty", () => {
+        const array = [] as Array<number>;
+
+        const action = () => array.first();
+
+        expect(action).toThrowError("The source sequence is empty");
+    });
+
+    test("first throws when no element satisfy a given predicate", () => {
+        const array = [1, 2, 3, 4, 5];
+        const predicate = (element: number) => element > 5;
+
+        const action = () => array.first(predicate);
+
+        expect(action).toThrowError("No element satisfies the condition in predicate");
+    });
+
+    test("firstOrUndefined returns first element of the array when the array contains elements and no predicate is provided", () => {
+        const array = [1, 2, 3, 4, 5];
+
+        const result = array.firstOrUndefined();
+
+        expect(result).toBe(1);
+    });
+
+    test("firstOrUndefined returns first element of the array that satisfied given predicate when such an element exists", () => {
+        const array = [1, 2, 3, 4, 5];
+        const predicate = (element: number) => element > 3;
+
+        const result = array.firstOrUndefined(predicate);
+
+        expect(result).toBe(4);
+    });
+
+    test("firstOrUndefined returns undefined when the array is empty", () => {
+        const array = [] as Array<number>;
+
+        const result = array.firstOrUndefined();
+
+        expect(result).toBe(undefined);
+    });
+
+    test("firstOrUndefined returns undefined when no element satisfy a given predicate", () => {
+        const array = [1, 2, 3, 4, 5];
+        const predicate = (element: number) => element > 5;
+
+        const result = array.firstOrUndefined(predicate);
+
+        expect(result).toBe(undefined);
+    });
+
+    test("groupBy returns map of array elements grouped by a given key selector", () => {
+        const element1 = { key: "1", value: 10 };
+        const element2 = { key: "2", value: 20 };
+        const element3 = { key: "2", value: 30 };
+        const element4 = { key: "1", value: 40 };
+        const array = [element1, element2, element3, element4];
+        const keySelector = (element: { key: string }) => element.key;
+
+        const result = array.groupBy(keySelector);
+
+        expect(result).toEqual(new Map([["1", [element1, element4]], ["2", [element2, element3]]]));
+    });
+
+    test("groupJoin returns proper array of result elements from joined arrays", () => {
+        const parent1 = { name: "A", value: 10 };
+        const parent2 = { name: "B", value: 20 };
+        const parent3 = { name: "C", value: 30 };
+        const child1 = { name: "W", parent: parent2 };
+        const child2 = { name: "X", parent: parent2 };
+        const child3 = { name: "Y", parent: parent3 };
+        const child4 = { name: "Z", parent: parent1 };
+        const outerArray = [parent1, parent2, parent3];
+        const innerArray = [child1, child2, child3, child4];
+        const outerKeySelector = (element: { name: string; value: number }) => element;
+        const innerKeySelector = (element: { name: string; parent: { name: string; value: number } }) => element.parent;
+        const resultSelector = (
+            outerElement: { name: string; value: number },
+            innerElements: Array<{ name: string; parent: { name: string; value: number } }>
+        ) => ({ parentName: outerElement.name, childrenNames: innerElements.map(x => x.name) });
+
+        const result = outerArray.groupJoin(innerArray, outerKeySelector, innerKeySelector, resultSelector);
+
+        expect(result).toEqual([
+            { parentName: "A", childrenNames: ["Z"] },
+            { parentName: "B", childrenNames: ["W", "X"] },
+            { parentName: "C", childrenNames: ["Y"] }
+        ]);
+    });
+
+    test("innerJoin returns proper array of result elements from joined arrays", () => {
+        const parent1 = { name: "A", value: 10 };
+        const parent2 = { name: "B", value: 20 };
+        const parent3 = { name: "C", value: 30 };
+        const child1 = { name: "W", parent: parent2 };
+        const child2 = { name: "X", parent: parent2 };
+        const child3 = { name: "Y", parent: parent3 };
+        const child4 = { name: "Z", parent: parent1 };
+        const outerArray = [parent1, parent2, parent3];
+        const innerArray = [child1, child2, child3, child4];
+        const outerKeySelector = (element: { name: string; value: number }) => element;
+        const innerKeySelector = (element: { name: string; parent: { name: string; value: number } }) => element.parent;
+        const resultSelector = (
+            outerElement: { name: string; value: number },
+            innerElement: { name: string; parent: { name: string; value: number } }
+        ) => ({ parentName: outerElement.name, childName: innerElement.name });
+
+        const result = outerArray.innerJoin(innerArray, outerKeySelector, innerKeySelector, resultSelector);
+
+        expect(result).toEqual([
+            { parentName: "A", childName: "Z" },
+            { parentName: "B", childName: "W" },
+            { parentName: "B", childName: "X" },
+            { parentName: "C", childName: "Y" }
+        ]);
+    });
+
+    test("sum returns sum of array elements transformed with a given selector", () => {
+        const array = [{ key: "1", value: 10 }, { key: "2", value: 20 }, { key: "3", value: 30 }, { key: "4", value: 40 }];
+        const selector = (element: { value: number }) => element.value;
+
+        const result = array.sum(selector);
+
+        expect(result).toBe(100);
+    });
+
+    test("sum returns sum of array elements when a selector is not provided and all array elements are of number type", () => {
+        const array = [10, 20, 30, 40];
+
+        const result = array.sum();
+
+        expect(result).toBe(100);
+    });
+
+    test("sum throws an error when a selector is not provided and not all array elements are of number type", () => {
+        const array = [10, 20, 30, 40, "a"];
+
+        const action = () => array.sum();
+
+        expect(action).toThrowError("All array elements must be numbers");
+    });
+
+    test("sum throws an error when the array is empty", () => {
+        const array = [] as Array<number>;
+
+        const action = () => array.sum();
+
+        expect(action).toThrowError("Sequence contains no elements");
+    });
+
+    test("where returns a new array with elements that satisfy a given condition", () => {
         const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        const predicate = (item: number) => item % 3 == 0;
+        const predicate = (element: number) => element > 3;
 
         const result = array.where(predicate);
 
-        expect(result).toEqual([3, 6, 9]);
+        expect(result).toEqual([4, 5, 6, 7, 8, 9, 10]);
+        expect(result).not.toBe(array);
     });
 });
--- src/Array.ts
+++ src/Array.ts
@@ -1,7 +1,212 @@
 interface Array<T> {
-    where(predicate: (item: T) => boolean): Array<T>;
+    aggregate<TAccumulate, TResult>(
+        seed: TAccumulate,
+        func: (accumulator: TAccumulate, element: T) => TAccumulate,
+        resultSelector: (accumulator: TAccumulate) => TResult
+    ): TResult;
+    all(predicate: (element: T) => boolean): boolean;
+    any(predicate: (element: T) => boolean): boolean;
+    append(element: T): Array<T>;
+    average(): number;
+    average(selector: (element: T) => number): number;
+    contains(elment: T): boolean;
+    count(): number;
+    defultIfEmpty(defaultValue: T): Array<T>;
+    distinct(): Array<T>;
+    elementAt(index: number): T;
+    elementAtOrUndefined(index: number): T | undefined;
+    except(second: Array<T>): Array<T>;
+    first(): T;
+    first(prdicate: (element: T) => boolean): T;
+    firstOrUndefined(): T | undefined;
+    firstOrUndefined(prdicate: (element: T) => boolean): T | undefined;
+    groupBy<TKey>(keySelector: (element: T) => TKey): Map<TKey, Array<T>>;
+    groupJoin<TInner, TKey, TResult>(
+        inner: Array<TInner>,
+        outerKeySelector: (element: T) => TKey,
+        innerKeySelector: (innerElement: TInner) => TKey,
+        resultSelector: (element: T, innerElements: Array<TInner>) => TResult
+    ): Array<TResult>;
+    innerJoin<TInner, TKey, TResult>(
+        inner: Array<TInner>,
+        outerKeySelector: (element: T) => TKey,
+        innerKeySelector: (innerElement: TInner) => TKey,
+        resultSelector: (element: T, innerElement: TInner) => TResult
+    ): Array<TResult>;
+    sum(): number;
+    sum(selector: (element: T) => number): number;
+    where(predicate: (element: T) => boolean): Array<T>;
 }
 
-Array.prototype.where = function<T>(this: Array<T>, predicate: (item: T) => boolean): Array<T> {
-    return this.filter(item => predicate(item));
+Array.prototype.aggregate = function<T, TAccumulate, TResult>(
+    this: Array<T>,
+    seed: TAccumulate,
+    func: (accumulator: TAccumulate, element: T) => TAccumulate,
+    resultSelector: (accumulator: TAccumulate) => TResult
+): TResult {
+    return resultSelector(this.reduce((accumulator: TAccumulate, element: T) => func(accumulator, element), seed));
+};
+
+Array.prototype.all = function<T>(this: Array<T>, predicate: (element: T) => boolean): boolean {
+    return this.every(element => predicate(element));
+};
+
+Array.prototype.any = function<T>(this: Array<T>, predicate: (element: T) => boolean): boolean {
+    return this.some(predicate);
+};
+
+Array.prototype.append = function<T>(this: Array<T>, element: T): Array<T> {
+    return [...this, element];
+};
+
+Array.prototype.average = function<T>(this: Array<T>, selector?: (element: T) => number): number {
+    if (this.length < 1) {
+        throw new Error("Sequence contains no elements");
+    }
+
+    return (selector !== undefined ? this.sum(selector) : this.sum()) / this.length;
+};
+
+Array.prototype.contains = function<T>(this: Array<T>, element: T): boolean {
+    return this.includes(element);
+};
+
+Array.prototype.count = function<T>(this: Array<T>): number {
+    return this.length;
+};
+
+Array.prototype.defultIfEmpty = function<T>(this: Array<T>, defaultValue: T): Array<T> {
+    return this.length > 0 ? this : [defaultValue];
+};
+
+Array.prototype.distinct = function<T>(this: Array<T>): Array<T> {
+    return [...new Set(this)];
+};
+
+Array.prototype.elementAt = function<T>(this: Array<T>, index: number): T {
+    if (index < 0 || index >= this.length) {
+        throw new Error("Index out of range");
+    }
+
+    return this[index];
+};
+
+Array.prototype.elementAtOrUndefined = function<T>(this: Array<T>, index: number): T | undefined {
+    if (index < 0 || index >= this.length) {
+        return undefined;
+    }
+
+    return this[index];
+};
+
+Array.prototype.except = function<T>(this: Array<T>, second: Array<T>): Array<T> {
+    return this.filter(element => !second.includes(element));
+};
+
+Array.prototype.first = function<T>(this: Array<T>, predicate?: (element: T) => boolean): T {
+    if (this.length < 1) {
+        throw Error("The source sequence is empty");
+    }
+    if (predicate === undefined) {
+        return this[0];
+    }
+
+    for (let i = 0; i < this.length; ++i) {
+        if (predicate(this[i])) {
+            return this[i];
+        }
+    }
+
+    throw Error("No element satisfies the condition in predicate");
+};
+
+Array.prototype.firstOrUndefined = function<T>(this: Array<T>, predicate?: (element: T) => boolean): T | undefined {
+    if (this.length < 1) {
+        return undefined;
+    }
+    if (predicate === undefined) {
+        return this[0];
+    }
+
+    for (let i = 0; i < this.length; ++i) {
+        if (predicate(this[i])) {
+            return this[i];
+        }
+    }
+
+    return undefined;
+};
+
+Array.prototype.groupBy = function<T, TKey>(keySelector: (element: T) => TKey): Map<TKey, Array<T>> {
+    return this.reduce((accumulator: Map<TKey, Array<T>>, element: T) => {
+        const key = keySelector(element);
+        const group = accumulator.get(key);
+        if (group === undefined) {
+            accumulator.set(key, [element]);
+        } else {
+            group.push(element);
+        }
+        return accumulator;
+    }, new Map<TKey, Array<T>>());
+};
+
+Array.prototype.groupJoin = function<T, TInner, TKey, TResult>(
+    this: Array<T>,
+    inner: Array<TInner>,
+    outerKeySelector: (element: T) => TKey,
+    innerKeySelector: (innerElement: TInner) => TKey,
+    resultSelector: (element: T, innerElements: Array<TInner>) => TResult
+): Array<TResult> {
+    const result = [] as Array<TResult>;
+
+    this.forEach((element: T) => {
+        const matchingInnerElements = [] as Array<TInner>;
+        inner.forEach((innerElement: TInner) => {
+            if (outerKeySelector(element) === innerKeySelector(innerElement)) {
+                matchingInnerElements.push(innerElement);
+            }
+        });
+        result.push(resultSelector(element, matchingInnerElements));
+    });
+
+    return result;
+};
+
+Array.prototype.innerJoin = function<T, TInner, TKey, TResult>(
+    inner: Array<TInner>,
+    outerKeySelector: (element: T) => TKey,
+    innerKeySelector: (innerElement: TInner) => TKey,
+    resultSelector: (element: T, innerElement: TInner) => TResult
+): Array<TResult> {
+    const result = [] as Array<TResult>;
+
+    this.forEach((element: T) => {
+        inner.forEach((innerElement: TInner) => {
+            if (outerKeySelector(element) === innerKeySelector(innerElement)) {
+                result.push(resultSelector(element, innerElement));
+            }
+        });
+    });
+
+    return result;
+};
+
+Array.prototype.sum = function<T>(this: Array<T>, selector?: (element: T) => number): number {
+    if (this.length < 1) {
+        throw new Error("Sequence contains no elements");
+    }
+
+    return selector !== undefined
+        ? this.reduce((accumulator: number, element: T) => accumulator + selector(element), 0)
+        : this.reduce((accumulator: number, element: T) => {
+              if (typeof element === "number") {
+                  return accumulator + element;
+              } else {
+                  throw Error("All array elements must be numbers");
+              }
+          }, 0);
+};
+
+Array.prototype.where = function<T>(this: Array<T>, predicate: (element: T) => boolean): Array<T> {
+    return this.filter(element => predicate(element));
 };
